f4011a11071af2035f33ab02f71f523d2ddf5595
B Kleinen <kleinen@infrastructure.de>
2018-08-17 15:11:25 +0200

    rails g migration AddContactToBooking contact:references persons:integer comment:text

---
+++ db/migrate/20180817131050_add_contact_to_booking.rb
@@ -0,0 +1,7 @@
+class AddContactToBooking < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :bookings, :contact, foreign_key: true
+    add_column :bookings, :persons, :integer
+    add_column :bookings, :comment, :text
+  end
+end
