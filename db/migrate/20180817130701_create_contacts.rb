3a0a5a34fd3f9602384d8c76593e327b5d85cf55
B Kleinen <kleinen@infrastructure.de>
2018-08-17 15:10:07 +0200

    rails g scaffold Contact street:string city:string postalCode:string phone:string email:string

---
+++ db/migrate/20180817130701_create_contacts.rb
@@ -0,0 +1,13 @@
+class CreateContacts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :contacts do |t|
+      t.string :street
+      t.string :city
+      t.string :postalCode
+      t.string :phone
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/views/contacts/edit.html.erb_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "contacts/edit", type: :view do
+  before(:each) do
+    @contact = assign(:contact, Contact.create!(
+      :street => "MyString",
+      :city => "MyString",
+      :postalCode => "MyString",
+      :phone => "MyString",
+      :email => "MyString"
+    ))
+  end
+
+  it "renders the edit contact form" do
+    render
+
+    assert_select "form[action=?][method=?]", contact_path(@contact), "post" do
+
+      assert_select "input[name=?]", "contact[street]"
+
+      assert_select "input[name=?]", "contact[city]"
+
+      assert_select "input[name=?]", "contact[postalCode]"
+
+      assert_select "input[name=?]", "contact[phone]"
+
+      assert_select "input[name=?]", "contact[email]"
+    end
+  end
+end
