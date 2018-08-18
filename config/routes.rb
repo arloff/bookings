2e8f79b12117b860e32393a2fcce8b92961a2280
B Kleinen <kleinen@infrastructure.de>
2018-08-18 02:44:51 +0200

    devise roles

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
   scope '/:locale' do
+    devise_for :users, controllers: {
+      registrations: 'users/registrations'
+    }
     # resources :people
     # resources :contacts
     resources :bookings
     root 'bookings#index'
+    get 'ical', to: 'bookings#ical'
   end
   root 'bookings#index'
-  get 'ical', to: 'bookings#ical'
   # see http://guides.rubyonrails.org/routing.html
 end
