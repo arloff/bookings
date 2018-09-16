7977dc1d3502d4ebb2995d7f047fe5ef859cf53d
B Kleinen <kleinen@infrastructure.de>
2018-09-17 00:26:00 +0200

    took out controls

--- app/javascript/packs/overview.jsx
+++ app/javascript/packs/overview.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import moment from 'moment';
 
 import {Calendar, CalendarControls} from 'react-yearly-calendar'
 
@@ -7,19 +8,114 @@ function onDatePicked(date) {
   alert(date);
 }
 
+
+
+
+class YearOfBookings extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const today = moment();
+
+    this.state = {
+      year: today.year(),
+      selectedDay: today,
+      selectedRange: [today, moment(today).add(15, 'day')],
+      forceFullWeeks: false,
+      showDaysOfWeek: true,
+      showTodayBtn: true,
+      showWeekSeparators: true,
+      selectRange: true,
+      firstDayOfWeek: 1 // monday
+    };
+  }
+
+  onPrevYear() {
+    this.setState(prevState => ({
+      year: prevState.year - 1
+    }));
+  }
+
+  onNextYear() {
+    this.setState(prevState => ({
+      year: prevState.year + 1
+    }));
+  }
+
+  goToToday() {
+    const today = moment();
+
+    this.setState({
+      selectedDay: today,
+      selectedRange: [today, moment(today).add(15, 'day')],
+      year: today.year()
+    });
+  }
+
+  datePicked(date) {
+    this.setState({
+      selectedDay: date,
+      selectedRange: [date, moment(date).add(15, 'day')]
+    });
+  }
+
+  rangePicked(start, end) {
+    this.setState({
+      selectedRange: [start, end],
+      selectedDay: start
+    });
+  }
+
+  render() {
+    const {
+      year,
+      showTodayBtn,
+      selectedDay,
+      showDaysOfWeek,
+      forceFullWeeks,
+      showWeekSeparators,
+      firstDayOfWeek,
+      selectRange,
+      selectedRange
+    } = this.state;
+
+    return (
+      <div>
+        <div id="calendar">
+          <CalendarControls
+            year={year}
+            showTodayButton={showTodayBtn}
+            onPrevYear={() => this.onPrevYear()}
+            onNextYear={() => this.onNextYear()}
+            goToToday={() => this.goToToday()}
+          />
+          <Calendar
+            year={year}
+            onPickDate={this.onDatePicked}
+            selectedDay={selectedDay}
+            showDaysOfWeek={showDaysOfWeek}
+            forceFullWeeks={forceFullWeeks}
+            showWeekSeparators={showWeekSeparators}
+            firstDayOfWeek={firstDayOfWeek}
+            selectRange={selectRange}
+            selectedRange={selectedRange}
+            onPickRange={(start, end) => this.rangePicked(start, end)}
+            onPickDate={date => this.onDatePicked(date)}
+
+          />
+        </div>
+
+      </div>
+    );
+  }
+}
+
+
+
 ReactDOM.render(
-  <div><p>huhu</p>
-  <Calendar
-   year={2018}
-   onPickDate={onDatePicked}
-   firstDayOfWeek={1}
+<div>
+  <YearOfBookings />
 
-
-  />
-  <CalendarControls
-    year={2018}
-   showTodayButton={true}
-  />
   </div>,
   document.getElementById('react-content')
 )
